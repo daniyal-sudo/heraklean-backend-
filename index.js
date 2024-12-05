e7247c46833c67f5e659af2139fa2d9537591221
M Daniyal SoftWare Engineer [React ,WordPress, PHP] <61905200+daniyal-sudo@users.noreply.github.com>
2024-12-06 06:11:01 +1100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ dotenv.config();
 
 import { connectDB } from "./Config/database.js";
 
-connectDB();
+
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port : ${ process.env.PORT }`);
 })
+
+connectDB();
